60c4594ad52ccac84f141b29af788dce8f2c07d9
Dana-Maria04 <caruntudana@yahoo.com>
2024-10-28 01:19:06 +0200

    adding cards for gameutils

---
+++ tema/src/gameutils/Cards.java
@@ -0,0 +1,97 @@
+package gameutils;
+
+import fileio.CardInput;
+
+import java.util.ArrayList;
+
+public class Cards {
+
+    private String name;
+    private int mana;
+    private int attackDamage;
+    private int health;
+    private String description;
+    private ArrayList<String> colors;
+
+    public Cards(final CardInput cardInput) {
+        //constructor for card attribute input
+        this.name = cardInput.getName();
+        this.mana = cardInput.getMana();
+        this.attackDamage = cardInput.getAttackDamage();
+        this.health = cardInput.getHealth();
+        this.description = cardInput.getDescription();
+        this.colors = cardInput.getColors();
+    }
+
+    public Cards(final Cards cards) {
+        // copy constructor
+        this.name = cards.getName();
+        this.mana = cards.getMana();
+        this.attackDamage= cards.getAttackDamage();
+        this.health = cards.getHealth();
+        this.description = cards.getDescription();
+        this.colors = cards.getColors();
+    }
+
+    public Cards() {
+        // default constructor
+        this.name = "";
+        this.mana = 0;
+        this.attackDamage=0;
+        this.health = 0;
+        this.description = "";
+        this.colors= new ArrayList<>();
+    }
+
+    // getters for cards
+    public String getName() {
+        return name;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    // setter for cards
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
+}
